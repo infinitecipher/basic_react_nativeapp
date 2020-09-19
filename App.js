c5fe0ffff22584519afe379ac445e22882230a04
Jhovel Reyes <jhovel@infinitecipher.local>
2020-09-19 20:55:10 +0300

    added modal and components

--- App.js
+++ App.js
@@ -1,24 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, View, Button, FlatList } from "react-native";
+
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
 export default function App() {
-  const [outputtext, setOutputText] = useState("Open up App.js to start working on your app!")
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [isAddMode, setIsAddMode] = useState(false);
+
+  const addGoalHandler = (goalTitle) => {
+    setCourseGoals((currentGoals) => [
+      ...currentGoals,
+      { key: Math.random().toString(), value: goalTitle },
+    ]);
+    setIsAddMode(false);
+  };
+
+  const deleteGoalHandler = (goalId) => {
+    setCourseGoals((currentGoals) => {
+      return currentGoals.filter((goal) => goal.key !== goalId);
+    });
+  };
+
+  const cancelGoalHandler = () => {
+    setIsAddMode(false);
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>{outputtext}</Text>
-      <StatusBar style="auto" />
-      <Button title="change text" onPress={() => setOutputText("Change text hi")}/>
+    <View style={styles.screen}>
+      <Button title="Add new Goal" onPress={() => setIsAddMode(true)} />
+      <GoalInput
+        visible={isAddMode}
+        onAddGoal={addGoalHandler}
+        onCancel={cancelGoalHandler}
+      />
+      <FlatList
+        data={courseGoals}
+        renderItem={(itemData) => (
+          <GoalItem
+            title={itemData.item.value}
+            onDelete={deleteGoalHandler.bind(this, itemData.item.key)}
+          />
+        )}
+      />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen: {
+    padding: 50,
   },
 });
